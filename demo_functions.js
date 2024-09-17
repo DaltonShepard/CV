1574f89ef62629709002f83bccbdbf662ca6720d
DaltonShepard <dcs19329@uga.edu>
2024-09-17 12:06:07 -0400

    JavaScript 1.1

--- demo_functions.js
+++ demo_functions.js
@@ -3,7 +3,24 @@ function multiplyBy2(input) {
 }   
 console.log(multiplyBy2(10));
 
-const multiplyBy3 = (input) => {
+const multiplyBy3 = function (input) {
   return input * 3;
 }
 console.log(multiplyBy3(10));
+
+const greet = (name) => ("Hey " + name);
+console.log(greet("Awesome Javascript programmers!"));
+
+const multiplyBy4 = (input) => {return input * 4};
+console.log(multiplyBy4(10));
+
+const multiplyBy5 = input => input * 5;
+console.log(multiplyBy5(10));
+
+let array = [1, 2, 3, 4, 5];
+console.log(array);
+let output = [];
+for (i in array) {
+    output[i] = multiplyBy2(array[i]);
+}
+console.log(output);
