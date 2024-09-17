a2344f84cbe5642f804f41ae0d94b5d30ed272c8
DaltonShepard <dcs19329@uga.edu>
2024-09-17 11:47:22 -0400

    JavaScript

---
+++ demo_functions.js
@@ -0,0 +1,9 @@
+function multiplyBy2(input) {
+  return input * 2;
+}   
+console.log(multiplyBy2(10));
+
+const multiplyBy3 = (input) => {
+  return input * 3;
+}
+console.log(multiplyBy3(10));
--- test.js
+++ test.js
@@ -1,1 +1,6 @@
 console.log("Hello, World!");
+let a = 10;
+console.log(a);
+a = "Hi Guys!";
+console.log(a);
+console.log(`typeof: ${typeof(a)} value: ${a}`);
